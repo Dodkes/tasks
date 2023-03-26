c7e60c18ad61e4d46f7520bc0548fbc1f657e171
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2023-03-27 01:11:14 +0200

    Left panel animation on document load

--- product-page/script.js
+++ product-page/script.js
@@ -1,4 +1,7 @@
 const scrollArrow = document.querySelector('.material-symbols-outlined')
+const [navbarFirstItem, navbarSecondItem, navbarThirdItem] = [document.getElementById('first-item-navbar'), document.getElementById('second-item-navbar'),document.getElementById('third-item-navbar')]
+const navBar = document.querySelector('.navbar')
+
 
 window.onscroll = () => { 
     if (document.documentElement.scrollTop > 50) {
@@ -7,3 +10,10 @@ window.onscroll = () => {
         scrollArrow.style.visibility = 'hidden'
     }
 }
+
+window.addEventListener('load', () => {
+    navbarFirstItem.style.cssText = 'animation: 0.5s slide-item forwards; animation-delay: 200ms'
+    navbarSecondItem.style.cssText = 'animation: 0.5s slide-item forwards; animation-delay: 300ms;'
+    navbarThirdItem.style.cssText = 'animation: 0.5s slide-item forwards; animation-delay: 400ms;'
+    navBar.style.cssText = 'animation: 600ms slide-navbar forwards'
+})
